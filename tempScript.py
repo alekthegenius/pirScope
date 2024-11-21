8fee3987febe9bcece9a58acf4d38f26d17bba6e
Alek V <alekvasek@icloud.com>
2024-11-21 11:23:14 -0600

    Update tempScript.py

--- tempScript.py
+++ tempScript.py
@@ -1,18 +1,16 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QPushButton, QVBoxLayout, QApplication, QWidget
 from picamera2.previews.qt import QPicamera2
 from picamera2 import Picamera2
 import datetime
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration())
+picam2.configure(picam2.create_preview_configuration(main={"size": (480, 480)}))
 
 app = QApplication([])
-qpicamera2 = QPicamera2(picam2, width=480, height=400, keep_ar=False)
+qpicamera2 = QPicamera2(picam2, width=480, height=480, keep_ar=False)
 
 qpicamera2.setWindowTitle("Qt Picamera2 App")
-qpicamera2.resize(480, 400)
+qpicamera2.resize(480, 480)
+qpicamera2.showFullScreen()
 
 picam2.start()
-qpicamera2.show()
 app.exec()
